30cf88f1cac606a45d376a7cc4aa7cd214d2e732
jiangjunhua <287787472@qq.com>
2019-01-22 12:34:46 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/a28778/coolweather/db/Province.java
@@ -0,0 +1,34 @@
+package com.example.a28778.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(){
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(){
+        this.provinceCode = provinceCode;
+    }
+
+}
