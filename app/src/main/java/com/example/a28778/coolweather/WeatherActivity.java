b6708dce53c1655cfb37b325d0fa6e37fdba91ae
jiangjunhua <287787472@qq.com>
2019-01-22 22:39:58 +0800

    修改 HeWeather— —> HeWeather5

--- app/src/main/java/com/example/a28778/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/a28778/coolweather/WeatherActivity.java
@@ -23,6 +23,7 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+    final static String API_KEY = "161a13c710dc42dbb1a1ef647c8765a4";
     private ScrollView weatherLayout;
     private TextView titleCity;
     private TextView titleUpdateTime;
@@ -100,7 +101,7 @@ public class WeatherActivity extends AppCompatActivity {
 
     private void requestWeather(final String weatherId) {
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" +
-                weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+                weatherId + "&key=161a13c710dc42dbb1a1ef647c8765a4";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
